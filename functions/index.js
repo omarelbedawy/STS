d5cd6f4b516fc86d07a2c390f07b34b38189c13e
Omar Elbedawy <elbedawyomar2009@gmail.com>
2025-10-18 18:58:37 +0000

    I faced this error when i tried to sign up as a student, however my emai

---
+++ functions/index.js
@@ -0,0 +1,29 @@
+const { onCall } = require("firebase-functions/v2/https");
+const { initializeApp } = require("firebase-admin/app");
+const { getAuth } = require("firebase-admin/auth");
+
+initializeApp();
+
+exports.setCustomUserClaims = onCall(async (request) => {
+  if (!request.auth) {
+    throw new functions.https.HttpsError(
+      "unauthenticated",
+      "The function must be called while authenticated."
+    );
+  }
+
+  const { uid, claims } = request.data;
+
+  try {
+    await getAuth().setCustomUserClaims(uid, claims);
+    return {
+      message: `Success! Custom claims set for user ${uid}.`,
+    };
+  } catch (error) {
+    console.error("Error setting custom claims:", error);
+    throw new functions.https.HttpsError(
+      "internal",
+      "An error occurred while setting custom claims."
+    );
+  }
+});
